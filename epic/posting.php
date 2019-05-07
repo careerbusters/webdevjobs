43c7402e8b1066f5b078469932d63adaf31cd963
Erik Young <youngblkraven@gmail.com>
2019-05-07 09:01:30 -0600

    created private classes for posting

---
+++ epic/posting.php
@@ -0,0 +1,24 @@
+<?php
+
+/** create class for table posting */
+
+class posting{
+
+	/** id and primary key from table	 */
+use Validate Uuid;
+private postingId;
+
+/** create private variables for posting which are postingContent, postingEmail, postingLocation, postingTitle, postingPay, postingCompanyName, postingDate, postingEndDate, postintRole*/
+
+	private $postingContent;
+	private $postingEmail;
+	private $postingLocation;
+	private $postingTitle;
+	private $postingPay;
+	private $postingCompanyName;
+	private $postingDate;
+	private $postingEndDate;
+	private $postingRole;
+
+
+}
