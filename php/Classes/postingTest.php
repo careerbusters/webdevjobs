073b8b6148769ad6028e11957691a4c1d34d8e0d
Erik Young <youngblkraven@gmail.com>
2019-05-09 14:21:13 -0600

    assemble table for schema

--- php/Classes/postingTest.php
+++ php/Classes/postingTest.php
@@ -3,8 +3,29 @@
 namespace careerbusters\webdevjobs;
 
 use PHPUnit\Framework\TestCase;
-use PHPUnit\DdUnit\TestCaseTrait;
+use PHPUnit\DbUnit\TestCaseTrait;
 use PHPUnit\DdUnit\QueryDataSet;
 use PHPUnit\DdUnit\Database\Connection;
-use PHPUnit\DdUnit\{Composite, Factory, Operation};
+use PHPUnit\DdUnit\Operation\{Composite, Factory, Operation};
 
+require_once (dirname(__DIR__, 3) . "/vendor/autoload.php");
+
+/** create abstract class to test */
+abstract class postingTest extends TestCase {
+	use TestCaseTrait;
+
+	/** PHPUnit database connection interface
+	 *@var Connection $connection
+	 */
+	protected $connection = null;
+
+	/** assemble the table from the schema and provides it to PHPUnit
+	 *
+	 *@return QueryDataSet assembled schema from PHPUnit
+	 */
+	public final function getDataSet () : QueryDataSet {
+		$dataset = new QueryDataSet($this->getConnection());
+		$dataset->addTable("profile");
+		
+	}
+}
