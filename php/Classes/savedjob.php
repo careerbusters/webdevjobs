9fa8935661a357c8f6231c632d22ecc2571cf8da
Natasha Lovato <nmarshlovato@cnm.edu>
2019-05-09 10:55:28 -0600

    created savedjob OOP file, added lines 1-27

---
+++ php/Classes/savedjob.php
@@ -0,0 +1,27 @@
+<?php
+namespace careerbusters\webdevjobs;
+require_once(dirname(__DIR__) . "/classes/autoload.php");
+
+use Ramsey\Uuid\Uuid;
+use tgray19\webdevjobs\ValidateDate;
+use tgray19\webdevjobs\ValidateUuid;
+
+/**
+ * Cross Section of a Saved Job
+ *
+ * This is a cross section of what is probably stored about a saved job. This entity is an entity that
+ * holds the keys to the other entities.
+ *
+ * @savedjob Natasha Lovato <nmarshlovato@cnm.edu>
+ * @version 1.0.0
+ **/
+class savedJob implements \JsonSerializable {
+	use ValidateDate;
+	use ValidateUuid;
+/**
+ * id and savedJob (foreign key)
+ * @var string Uuid $savedJobId
+ **/
+private $savedJobId;
+
+}
