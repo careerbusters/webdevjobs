758c6ffed5f2d8771955dc81c15bcc0dc3dbc0f2
Erik Young <youngblkraven@gmail.com>
2019-05-09 13:47:01 -0600

    created branch postingTest

---
+++ php/Classes/postingTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace careerbusters\webdevjobs;
+
+use PHPUnit\Framework\TestCase;
+use PHPUnit\DdUnit\TestCaseTrait;
+use PHPUnit\DdUnit\QueryDataSet;
+use PHPUnit\DdUnit\Database\Connection;
+use PHPUnit\DdUnit\{Composite, Factory, Operation};
+
