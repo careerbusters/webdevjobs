0274daf177cb2e86e0a9ce2b80ed64abdfbbeaa9
Natasha Lovato <nmarshlovato@cnm.edu>
2019-05-09 20:33:30 -0600

    added lines 1-8

---
+++ php/Classes/role.php
@@ -0,0 +1,9 @@
+<?php
+namespace careerbusters\webdevjobs;
+require_once(dirname(__DIR__) . "/classes/autoload.php");
+
+use http\Exception\BadUrlException;
+use Ramsey\Uuid\Uuid;
+use tgray19\webdevjobs\ValidateDate;
+use tgray19\webdevjobs\ValidateUuid;
+
