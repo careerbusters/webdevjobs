a326acd885b52dd284c22bd5020479d3bac8dd8f
Natasha Lovato <nmarshlovato@cnm.edu>
2019-05-09 20:40:20 -0600

    added class

--- php/Classes/role.php
+++ php/Classes/role.php
@@ -7,3 +7,15 @@ use Ramsey\Uuid\Uuid;
 use tgray19\webdevjobs\ValidateDate;
 use tgray19\webdevjobs\ValidateUuid;
 
+/**
+ * Cross Section of a Saved Job
+ *
+ * This is a cross section of what is probably stored about a saved job. This entity is an entity that
+ * holds the keys to the other entities.
+ *
+ * @savedjob Natasha Lovato <nmarshlovato@cnm.edu>
+ * @version 1.0.0
+ **/
+class savedJobPosting implement \JsonSerializable; {
+	use ValidateDate;
+	use ValidateUuid;
