f45b406a153129916ab8ff32a2ae05b90bcfe442
Erik Young <youngblkraven@gmail.com>
2019-05-13 10:54:15 -0600

    added template to run test

---
+++ php/Classes/PHPUnit-Base-Class.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace careerbusters\webdevjobs;
+
+use PHPUnit\Framework\TestCase;
+use PHPUnit\DbUnit\TestCaseTrait;
+use PHPUnit\DdUnit\QueryDataSet;
+use PHPUnit\DdUnit\Database\Connection;
+use PHPUnit\DdUnit\Operation\{Composite, Factory, Operation};
+
+// grab the encrypted properties file
+require_once("/etc/apache2/capstone-mysql/Secret.php");
+
+require_once (dirname(__DIR__, 3) . "/vendor/autoload.php");
+
+/** create abstract class to test */
+abstract class postingTest extends TestCase {
+	use TestCaseTrait;
+
+	/** PHPUnit database connection interface
+	 * @var Connection $connection
+	 */
+	protected $connection = null;
+
+	/** assemble the table from the schema and provides it to PHPUnit
+	 *
+	 * @return QueryDataSet assembled schema from PHPUnit
+	 */
+	public final function getDataSet(): QueryDataSet {
+		$dataset = new QueryDataSet($this->getConnection());
+		$dataset->addTable("job");
+		$dataset->addTable("role");
+		$dataset->addTable("profile");
+		$dataset->addTable("posting");
+	}
+}
+
+/**
+ * templates for running before each test
+ * @return Composite array containing delete and insert commnds
+**/
+public final function getSetupOperation() : Composite {
+	return new Composite([
+		Factory::DELETE_ALL(),
+		FACTORY::INSERT()
+	]);
+}
