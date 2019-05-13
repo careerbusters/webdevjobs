0c8312fa68404a21b25061dcbfb24146b5dcc376
Erik Young <youngblkraven@gmail.com>
2019-05-13 11:30:17 -0600

    adding namespace to postingtest

--- php/Classes/postingTest.php
+++ php/Classes/postingTest.php
@@ -1,31 +1,10 @@
 <?php
+namespace \Test;
 
-namespace careerbusters\webdevjobs;
+use {posting};
 
-use PHPUnit\Framework\TestCase;
-use PHPUnit\DbUnit\TestCaseTrait;
-use PHPUnit\DdUnit\QueryDataSet;
-use PHPUnit\DdUnit\Database\Connection;
-use PHPUnit\DdUnit\Operation\{Composite, Factory, Operation};
+// grab the class under scrutiny
+require_once(dirname(__DIR__) . "/classes/autoload.php");
 
-require_once (dirname(__DIR__, 3) . "/vendor/autoload.php");
-
-/** create abstract class to test */
-abstract class postingTest extends TestCase {
-	use TestCaseTrait;
-
-	/** PHPUnit database connection interface
-	 *@var Connection $connection
-	 */
-	protected $connection = null;
-
-	/** assemble the table from the schema and provides it to PHPUnit
-	 *
-	 *@return QueryDataSet assembled schema from PHPUnit
-	 */
-	public final function getDataSet () : QueryDataSet {
-		$dataset = new QueryDataSet($this->getConnection());
-		$dataset->addTable("profile");
-		
-	}
-}
+// grab the uuid generator
+require_once(dirname(__DIR__, 2) . "/lib/uuid.php");
