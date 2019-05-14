6f3f915aed5aad282c366797ba77ec1e0674fcc8
Erik Young <youngblkraven@gmail.com>
2019-05-14 16:39:38 -0600

    added testing for multiple attributes

--- php/Classes/Test/PostingTest.php
+++ php/Classes/Test/PostingTest.php
@@ -4,15 +4,49 @@ use CareerBusters\WebDevJobs\{posting};
 // grab the class under scrutiny
 require_once(dirname(__DIR__) . "/autoload.php");
 // grab the uuid generator
-require_once(dirname(__DIR__, 2) . "/ValidateUuid.php");
+require_once(dirname(__DIR__, 2) . "/lib/uuid.php");
 
 /**
  *Full PHPUnit test for the Posting class
  */
 class PostingTest extends DataDesignTest {
-	/** Posting that created the 
-	 *
+	/** Posting company name that created the Posting
+	 * @var $postingCompanyName
 	 */
-	protected $posting = null;
-
+	protected $postingCompanyName;
+	/**
+	 *posting content that created the Posting
+	 *@var $postingContent
+	 */
+	protected $postingContent = "PHPUnit test passing";
+	/**
+	 timestamp of the posting
+	 *@var \DateTime $postingDate
+	 */
+	protected $postingDate;
+	/**
+	posting email address of the person posting
+	 *@var $postingEmail
+	 */
+	protected $postingEmail;
+	/**
+	timestamp of the posting
+	 *@var \DateTime $postingEndDate
+	 */
+	protected $postingEndDate;
+	/**
+	posting location of the person posting
+	 *@var $postingLocation
+	 */
+	protected $postingLocation;
+	/**
+	posting pay for the posting
+	 *@var $postingPay
+	 */
+	protected $postingPay;
+	/**
+	posting title of the posting
+	 *@var $postingTitle
+	 */
+	protected $postingTitle;
 }
