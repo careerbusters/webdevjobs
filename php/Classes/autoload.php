936e033768b9ed0c08f429372c7b4ccbcfe5f09e
Trystan Gray <trystangray7@gmail.com>
2019-05-14 14:28:51 -0600

    added \

--- php/Classes/autoload.php
+++ php/Classes/autoload.php
@@ -15,7 +15,7 @@ spl_autoload_register(function($class) {
 	 * prefix: the prefix for all the classes (i.e., the namespace)
 	 * baseDir: the base directory for all classes (default = current directory)
 	 **/
-	$prefix = "CareerBusters\WebDevJobs";
+	$prefix = "CareerBusters\\WebDevJobs";
 	$baseDir = __DIR__;
 	// does the class use the namespace prefix?
 	$len = strlen($prefix);
