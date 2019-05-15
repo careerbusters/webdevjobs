2b970cf22b18197973980fb4f8345d995a44ebe8
Natasha Lovato <nmarshlovato@cnm.edu>
2019-05-14 22:21:08 -0600

    added test inserting a role

--- php/Classes/Test/RoleTest.php
+++ php/Classes/Test/RoleTest.php
@@ -28,10 +28,34 @@ public final function setUp()  : void {
 		$role->insert($this->getPDO());
 
 		// grab the data from mySQL and enforce the fields match our expectations
-		$pdoROLE = Tweet::getRoleByRoleId($this->getPDO(), $role->getRoleId());
+		$pdoROLE = Role\::getRoleByRoleId($this->getPDO(), $role->getRoleId());
 		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("role"));
 		$this->assertEquals($pdoRole->getRoleId(), $roleId);
 		$this->assertEquals($pdoRole->getRoleId(), $this->role->getRoleId());
 		$this->assertEquals($pdoRole->getRoleName(), $this->VALID_ROLENAME);
 	}
 }
+
+/**
+ * test inserting a Role, editing it, and then updating it
+ **/
+	public function testUpdateValidRole() : void {
+	// count the number of rows and save it for later
+	$numRows = $this->getConnection()->getRowCount("role");
+
+	// create a new Role and insert to into mySQL
+	$roleId = generateUuidV4();
+	$role = new Role($roleId, $this->role->getRoleId(), $this->VALID_ROLEID, $this->VALID_ROLENAME);
+	$role->insert($this->getPDO());
+
+	// edit the ROLE and update it in mySQL
+	$role->setRoleId($this->VALID_ROLEID2);
+	$ROLE->update($this->getPDO());
+
+	// grab the data from mySQL and enforce the fields match our expectations
+	$pdoRole = Role::getRoleByRoleId($this->getPDO(), $role->getRoleId());
+	$this->assertEquals($pdoRole->getRoleId(), $roleId);
+	$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("role"));
+	$this->assertEquals($pdoRole->getRoleId(), $this->role->getRoleId());
+	$this->assertEquals($pdoRole->getRoleName(), $this->VALID_ROLENAME2);
+	}
