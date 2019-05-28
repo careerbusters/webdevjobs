bd5f0812180ac4956e430b502aaaf1543bb467cf
Erik Young <youngblkraven@gmail.com>
2019-05-28 17:07:33 -0600

    adding API info

--- php/public_html/API/signup/index.php
+++ php/public_html/API/signup/index.php
@@ -5,16 +5,17 @@ require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
 require_once dirname(__DIR__, 3) . "/php/lib/uuid.php";
 require_once("/etc/apache2/capstone-mysql/Secrets.php");
 
-use Edu\Cnm\DataDesign\{
+use CareerBusters\WebDevJobs;{
+
 	// we only use the profile class for testing purposes
-	Profile
+	Profile;
 };
 
 
 /**
- * api for the Tweet class
+ * api for the Profile class
  *
- * @author {} <valebmeza@gmail.com>
+ * @author {} <youngblkraven@gmail.com>
  * @coauthor Derek Mauldin <derek.e.mauldin@gmail.com>
  **/
 
@@ -31,54 +32,42 @@ $reply->data = null;
 try {
 	//grab the mySQL connection
 	$secrets = new \Secrets("/etc/apache2/capstone-mysql/ddctwitter.ini");
-
+	$pdo = $secrets->getPdoObject();
 
 	//determine which HTTP method was used
 	$method = $_SERVER["HTTP_X_HTTP_METHOD"] ?? $_SERVER["REQUEST_METHOD"];
 
 	//sanitize input
-	$id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_STRING,FILTER_FLAG_NO_ENCODE_QUOTES);
-	$tweetProfileId = filter_input(INPUT_GET, "tweetProfileId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-	$tweetContent = filter_input(INPUT_GET, "tweetContent", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$profileId = filter_input(INPUT_GET, "profileId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+
 
 	//make sure the id is valid for methods that require it
 	if(($method === "DELETE" || $method === "PUT") && (empty($id) === true)) {
 		throw(new InvalidArgumentException("id cannot be empty or negative", 405));
 	}
-else if($method === "POST") {
 
-	// enforce the user has a XSRF token
-	verifyXsrf();
+	else if($method === "POST") {
 
-	//  Retrieves the JSON package that the front end sent, and stores it in $requestContent. Here we are using file_get_contents("php://input") to get the request from the front end. file_get_contents() is a PHP function that reads a file into a string. The argument for the function, here, is "php://input". This is a read only stream that allows raw data to be read from the front end request which is, in this case, a JSON package.
-	$requestContent = file_get_contents("php://input");
+			// enforce the user has a XSRF token
+			verifyXsrf();
 
-	// This Line Then decodes the JSON package and stores that result in $requestObject
-	$requestObject = json_decode($requestContent);
+			//  Retrieves the JSON package that the front end sent, and stores it in $requestContent. Here we are using file_get_contents("php://input") to get the request from the front end. file_get_contents() is a PHP function that reads a file into a string. The argument for the function, here, is "php://input". This is a read only stream that allows raw data to be read from the front end request which is, in this case, a JSON package.
+			$requestContent = file_get_contents("php://input");
 
-	// make sure tweet date is accurate (optional field)
-	if(empty($requestObject->profileDate) === true) {
-		$requestObject->profileDate = null;
-	} else {
-		// if the date exists, Angular's milliseconds since the beginning of time MUST be converted
-		$profileDate = DateTime::createFromFormat("U.u", $requestObject->profileDate / 1000);
-		if($profileDate === false) {
-			throw(new RuntimeException("invalid profile date", 400));
-		}
-		$requestObject->profileDate = $profileDate;
-	}}
-else if($method === "POST") {
+			// This Line Then decodes the JSON package and stores that result in $requestObject
+			$requestObject = json_decode($requestContent);
 
-	// enforce the user is signed in
-	if(empty($_SESSION["profile"]) === true) {
-		throw(new \InvalidArgumentException("you must be logged in to create a profile", 403));
-	}
 
-	// create new profile and insert into the database
-	$profile = new Profile(generateUuidV4(), $_SESSION["profile"]->getProfileId, null);
-	$profile->insert($pdo);
+			// make sure tweet date is accurate (optional field)
+			if(empty($requestObject->profileDate) === true) {
+				$requestObject->profileDate = null;
+			} else {
+				// if the date exists, Angular's milliseconds since the beginning of time MUST be converted
+				$profileDate = DateTime::createFromFormat("U.u", $requestObject->profileDate / 1000);
+				if($profileDate === false) {
+					throw(new RuntimeException("invalid profile date", 400));
+				}
+				$requestObject->profileDate = $profileDate;
 
-	// update reply
-	$reply->message = "profile created OK";
-}
-}
+
