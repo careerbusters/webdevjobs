15f6ebfbf093c91d14f358f07383d515119f6dd4
Erik Young <youngblkraven@gmail.com>
2019-05-29 08:54:12 -0600

    added swift to composer

--- php/public_html/API/signup/index.php
+++ php/public_html/API/signup/index.php
@@ -5,7 +5,10 @@ require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
 require_once dirname(__DIR__, 3) . "/php/lib/uuid.php";
 require_once("/etc/apache2/capstone-mysql/Secrets.php");
 
-use CareerBusters\WebDevJobs;{
+use CareerBusters\WebDevJobs;
+use CareerBusters\WebDevJobs\Profile;
+
+{
 
 	// we only use the profile class for testing purposes
 	Profile;
@@ -15,8 +18,7 @@ use CareerBusters\WebDevJobs;{
 /**
  * api for the Profile class
  *
- * @author {} <youngblkraven@gmail.com>
- * @coauthor Derek Mauldin <derek.e.mauldin@gmail.com>
+ * @author  <youngblkraven@gmail.com>
  **/
 
 //verify the session, start if not active
@@ -31,43 +33,117 @@ $reply->data = null;
 
 try {
 	//grab the mySQL connection
-	$secrets = new \Secrets("/etc/apache2/capstone-mysql/ddctwitter.ini");
+	$secrets = new \Secrets("/etc/apache2/capstone-mysql/CareerBusters");
 	$pdo = $secrets->getPdoObject();
 
 	//determine which HTTP method was used
 	$method = $_SERVER["HTTP_X_HTTP_METHOD"] ?? $_SERVER["REQUEST_METHOD"];
+	if($method ==="POST"){
 
-	//sanitize input
-	$id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-	$profileId = filter_input(INPUT_GET, "profileId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-
-
-	//make sure the id is valid for methods that require it
-	if(($method === "DELETE" || $method === "PUT") && (empty($id) === true)) {
-		throw(new InvalidArgumentException("id cannot be empty or negative", 405));
+		//have to decode json and turn it into a php object
+		$requestContent = file_get_contents("php://input");
+		$requestObject = json_decode($requestContent);
 	}
+	$profileId = generateUuidV4();
+	$profileActivationToken = bin2hex(random_bytes(16));
+	//profile Bio is a required field
+	if (empty($requestObject->profileBio) === true) {
+		throw(new \InvalidArgumentException("No profile Bio is present", 405));
+	}
+	//check profile email is a required field
+	if (empty($requestObject->profileEmail) === true) {
+		throw(new \InvalidArgumentException("No profile email is present", 405));
+	}
+	$newProfileHash = password_hash($requestObject->profilePassword, PASSWORD_ARGON2I, ["time_cost" => 384]);
 
-	else if($method === "POST") {
+	//verify that profile password is present
+	if(empty($requestObject->profileImage) === true) {
+		throw(new \InvalidArgumentException("no profile image present", 405));
+	}
+//verify that profile password is present
+	if(empty($requestObject->profileLocation) === true) {
+		throw(new \InvalidArgumentException("Location must be Albuquerque", 405));
+	}
+//verify that profile password is present
+	if(empty($requestObject->profileUsername) === true) {
+		throw(new \InvalidArgumentException("Must input valid username", 405));
+	}
+	// profile object needs to be created and prepare to insert into the database
+	$profile = new Profile($profileId, $profileActivationToken, $requestObject->profileEmail, $requestObject->profileImage, $requestObject->profileLocation, $requestObject->profileUsername, $requestObject->profileBio, $newProfileHash);
+	//insert the profile into the database
+	$profile->insert($pdo);
+	//compose the email message to send with the activation token
+	$messageSubject = "One step closer -- Account Activation";
+	//building the activation link that can travel to another server and still work. This is the link that will be clicked to confirm to the account.
+	//make sure URL is /public_html/api/activation/$activation
+	$basePath = dirname($_SERVER["SCRIPT_NAME"], 3);
+	//create the path
+	$urlGlue = $basePath . "/api/activation/?activation=" . $profileActivationToken;
+	//create the redirect link
+	$confirmLink = "https://" . $_SERVER["SERVER_NAME"] . $urlGlue;
+	//compose message to send with email
+	$message = <<< EOF
+<h2> Welcome to Whats-For-Lunch!</h2>
+<p>Sign in to Save your Favorites and confirm your account</p>
+<p><a href="$confirmLink">$confirmLink</a></p>
+EOF;
+	//created a swift email
+	$swiftMessage = new Swift_Message();
+	// attach the sender to the message
+	//this takes the form of an associative array where the email is the key to a real name
+	$swiftMessage->setFrom(["youngblkraven@gmail.com" => "Youngblkraven"]);
+	/**
+	 * attach recipients to the message
+	 * notice this is an array that can include or omit the recipient's name
+	 * use the recipient's real name where possible;
+	 * this reduces the probability of the email is marked as spam
+	 */
+	//define who the recipient is
+	$recipients = [$requestObject->profileEmail];
+	//set the recipient to the swift message
+	$swiftMessage->setTo($recipients);
+	//attach the subject line to the email message
+	$swiftMessage->setSubject($messageSubject);
+	/**
+	 * attach the message to the email
+	 * set two versions of the message: a html formatted version and a filter_var() of the message, plain text
+	 * notice the tactic used is to display the entire $confirmLink to plain text
+	 * this let users who are not viewing the html content to still access the link
+	 */
+	//attach the html verizon for the message
+	$swiftMessage->setBody($message, "text/html");
+	//attach the plain text version of the message
+	$swiftMessage->addPart(html_entity_decode($message), "text/plain");
+	/**
+	 * send the Email via SMTp; the SMTP server here is configured to relay everything upstream via CNM
+	 * ths default may or may not be available on all web hosts; consult their documentation/support for details
+	 * SwiftMailer support many different transport methods; SMTP was chosen because it's the most compatible and has the best error handling
+	 * @see https://swiftmailer.org/docs/sending.html Sending Messages - Documentation - SwiftMailer
+	 */
+	//setup SMTP
+	$smtp = new Swift_SmtpTransport("localhost", 25);
 
-			// enforce the user has a XSRF token
-			verifyXsrf();
-
-			//  Retrieves the JSON package that the front end sent, and stores it in $requestContent. Here we are using file_get_contents("php://input") to get the request from the front end. file_get_contents() is a PHP function that reads a file into a string. The argument for the function, here, is "php://input". This is a read only stream that allows raw data to be read from the front end request which is, in this case, a JSON package.
-			$requestContent = file_get_contents("php://input");
-
-			// This Line Then decodes the JSON package and stores that result in $requestObject
-			$requestObject = json_decode($requestContent);
-
-
-			// make sure tweet date is accurate (optional field)
-			if(empty($requestObject->profileDate) === true) {
-				$requestObject->profileDate = null;
-			} else {
-				// if the date exists, Angular's milliseconds since the beginning of time MUST be converted
-				$profileDate = DateTime::createFromFormat("U.u", $requestObject->profileDate / 1000);
-				if($profileDate === false) {
-					throw(new RuntimeException("invalid profile date", 400));
-				}
-				$requestObject->profileDate = $profileDate;
-
+	$mailer = new Swift_Mailer($smtp);
+	//send the message
+	$numSent = $mailer->send($swiftMessage, $failedRecipients);
+	/**
+	 * the send method returns the number of recipients that accepted the Email
+	 * so, if the number attempted is not the number accepted, this is an Exception
+	 */
+	if ($numSent !== count($recipients)) {
+		//the $failedRecipients parameter passed in the send () method now contains an array of the Emails that failed
+		throw(new RuntimeException("Unable to send mail", 400));
+	}
+	// update reply
+	$reply->message = "";
+} else {
+	throw(new InvalidArgumentException("invalid http request"));
+}
+} catch (\Exception | \TypeError $exception) {
+	$reply->status = $exception->getCode();
+	$reply->message = $exception->getMessage();
+	$reply->trace = $exception->getTraceAsString();
+}
+header("Content-type: application/json");
+echo json_encode($reply);
 
