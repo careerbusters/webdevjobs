184f9688cb550eab93fcdf90729bd37e157710f9
Trystan Gray <trystangray7@gmail.com>
2019-05-29 11:26:30 -0600

    added spacing

--- php/public_html/API/login/index.php
+++ php/public_html/API/login/index.php
@@ -1,9 +1,9 @@
 <?php
-require_once dirname(__DIR__, 3) . "/php/Classes/autoload.php";
-require_once("/etc/apache2/capstone-mysql/Secrets.php");
-require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
-require_once dirname(__DIR__, 3) . "/php/lib/jwt.php";
-require_once dirname(__DIR__, 3) . "/php/lib/uuid.php";
+require_once dirname(__DIR__, 3) . "/vendor/autoload.php";
+require_once dirname(__DIR__, 3) . "/Classes/autoload.php";
+require_once dirname(__DIR__, 3) . "/lib/xsrf.php";
+require_once dirname(__DIR__, 3) . "/lib/jwt.php";
+require_once dirname(__DIR__, 3) . "/lib/uuid.php";
 require_once("/etc/apache2/capstone-mysql/Secrets.php");
 use CareerBusters\WebDevJobs\Profile;
 /**
@@ -16,34 +16,42 @@ use CareerBusters\WebDevJobs\Profile;
 $reply = new stdClass();
 $reply->status = 200;
 $reply->data = null;
+
 try {
 	//start session
 	if(session_status() !== PHP_SESSION_ACTIVE) {
 		session_start();
 	}
+
 	//grab mySQL statement
 	$secrets = new \Secrets("/etc/apache2/capstone-mysql/busters.ini");
 	$pdo = $secrets->getPdoObject();
+
 	//determine which HTTP method is being used
 	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+
 	//If method is post handle the sign in logic
 	if($method === "POST") {
+
 		//make sure the XSRF Token is valid
 		verifyXsrf();
+
 		//process the request content and decode the json object into a php object
 		$requestContent = file_get_contents("php://input");
 		$requestObject = json_decode($requestContent);
+
 		//check to make sure the password and email field is not empty.s
 		if(empty($requestObject->profileEmail) === true) {
-			throw(new \InvalidArgumentException("email address not provided.", 401));
+			throw(new \InvalidArgumentException("Must Provide email", 401));
 		} else {
 			$profileEmail = filter_var($requestObject->profileEmail, FILTER_SANITIZE_EMAIL);
 		}
 		if(empty($requestObject->profilePassword) === true) {
-			throw(new \InvalidArgumentException("Must enter a password.", 401));
+			throw(new \InvalidArgumentException("Must enter password.", 401));
 		} else {
 			$profilePassword = $requestObject->profilePassword;
 		}
+
 		//grab the profile from the database by the email provided
 		$profile = Profile::getProfileByProfileEmail($pdo, $profileEmail);
 		if(empty($profile) === true) {
@@ -51,28 +59,34 @@ try {
 		}
 		$profile->setProfileActivationToken(null);
 		$profile->update($pdo);
+
 		//verify hash is correct
 		if(password_verify($requestObject->profilePassword, $profile->getProfileHash()) === false) {
 			throw(new \InvalidArgumentException("Password or email is incorrect.", 401));
 		}
+
 		//grab profile from database and put into a session
 		$profile = Profile::getProfileByProfileId($pdo, $profile->getProfileId());
 		$_SESSION["profile"] = $profile;
+
 		//create the Auth payload
 		$authObject = (object) [
 			"profileId" =>$profile->getProfileId(),
 			"profileUsername" => $profile->getProfileUsername()
 		];
+
 		// create and set th JWT TOKEN
 		setJwtAndAuthHeader("auth",$authObject);
 		$reply->message = "Sign in was successful.";
 	} else {
 		throw(new \InvalidArgumentException("Invalid HTTP method request", 418));
 	}
+
 	// if an exception is thrown update the
 } catch(Exception | TypeError $exception) {
 	$reply->status = $exception->getCode();
 	$reply->message = $exception->getMessage();
 }
+
 header("Content-type: application/json");
 echo json_encode($reply);
