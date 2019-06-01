fd390f5b583ab120d9879199e2b4ca59598cefa0
Erik Young <youngblkraven@gmail.com>
2019-06-01 00:16:23 -0600

    job-posting API

--- php/public_html/API/job-posting/index.php
+++ php/public_html/API/job-posting/index.php
@@ -65,7 +65,7 @@ try {
 		// Retrieves the JSON package that the front end sent, and stores it in $requestContent. Here we are using file_get_contents("php://input") to get the request from the front end. file_get_contents() is a PHP function that reads a file into a string. The argument for the function, here, is "php://input". This is a read only stream that allows raw data to be read from the front end request which is, in this case, a JSON package.
 		$requestObject = json_decode($requestContent);
 		// This Line Then decodes the JSON package and stores that result in $requestObject
-		
+
 		// retrieve the posting to update
 		$posting = Posting::getPostingByPostingId($pdo, $id);
 		if($posting === null) {
