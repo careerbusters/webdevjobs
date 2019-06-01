904b436a632a8ef7252fabc2100102efae4056a9
Erik Young <youngblkraven@gmail.com>
2019-06-01 12:34:33 -0600

    added xsrf to signup

--- php/public_html/API/signup/index.php
+++ php/public_html/API/signup/index.php
@@ -5,7 +5,7 @@ require_once dirname(__DIR__, 3) . "/lib/xsrf.php";
 require_once dirname(__DIR__, 3) . "/lib/uuid.php";
 require_once("/etc/apache2/capstone-mysql/Secrets.php");
 
-use CareerBusters\WebDevJobs\Profile;
+use CareerBusters\WebDevJobs\{Profile, Role};
 
 /**
  * api for the Profile class
@@ -29,16 +29,14 @@ try {
 	$pdo = $secrets->getPdoObject();
 
 	//determine which HTTP method was used
-	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
-	if($method === "POST") {
+	$method = $_SERVER["HTTP_X_HTTP_METHOD"] ?? $_SERVER["REQUEST_METHOD"];
 
+	if($method === "POST") {
+		verifyXsrf();
 		//have to decode json and turn it into a php object
 		$requestContent = file_get_contents("php://input");
 		$requestObject = json_decode($requestContent);
 		//verify that profile password is present
-		if(empty($requestObject->profileRoleId) === true) {
-			throw(new \InvalidArgumentException("please enter a role, your role appears empty", 405));
-		}
 		//profile Bio is a required field
 		if(empty($requestObject->profileBio) === true) {
 			throw(new \InvalidArgumentException("profile Bio is empty", 405));
@@ -47,12 +45,11 @@ try {
 		if(empty($requestObject->profileEmail) === true) {
 			throw(new \InvalidArgumentException("No profile email is present", 405));
 		}
-		/*
+
 			//verify that profile Image is present
-			if(empty($requestObject->profileImage) === true) {
+		if(empty($requestObject->profileImage) === true) {
 				throw(new \InvalidArgumentException("no profile image present", 405));
 			}
-			*/
 //verify that profile location is present
 		if(empty($requestObject->profileLocation) === true) {
 			throw(new \InvalidArgumentException("Location must be Albuquerque", 405));
@@ -69,6 +66,10 @@ try {
 		if(empty($requestObject->profilePasswordConfirm) === true) {
 			throw(new \InvalidArgumentException("Must input valid password", 405));
 		}
+		//verify that profile password is present
+		if(empty($requestObject->roleName) === true) {
+			throw(new \InvalidArgumentException("must input a valid name", 405));
+		}
 
 		/*do the values below  get assigned on sign up or after activation?
 		$profileUsername = $_SERVER['HTTP_Profile_Username'];
@@ -79,21 +80,24 @@ try {
 		//do the values below for signup and activate
 		$hash = password_hash($requestObject->profilePassword, PASSWORD_ARGON2I, ["time_cost" => 384]);
 		$profileActivationToken = bin2hex(random_bytes(16));
-		$profileId = null;
+		$roleId = generateUuidV4();
+		$profileId = generateUuidV4();
 		// profile object needs to be created and prepare to insert into the database
-		$profile = new Profile($profileId, $_SESSION["profile"]->getProfileId, $profileActivationToken, $requestObject->profileBio, $requestObject->profileEmail, $hash, $requestObject->profileImage, $requestObject->profileLocation, $requestObject->profileUsername,);
-
+		$role = new Role($roleId, $requestObject->roleName);
+		$profile = new Profile($profileId, $roleId, $profileActivationToken, $requestObject->profileBio, $requestObject->profileEmail, $hash, null, $requestObject->profileLocation, $requestObject->profileUsername);
 		//insert the profile into the database
+		$role->insert($pdo);
 		$profile->insert($pdo);
+
 		//compose the email message to send with the activation token
-		$messageSubject = "One step closer -- Account Activation";
+		$messageSubject = "One step closer to Account Activation";
 		//building the activation link that can travel to another server and still work. This is the link that will be clicked to confirm to the account.
 		//make sure URL is /public_html/api/activation/$activation
 		$basePath = dirname($_SERVER["SCRIPT_NAME"], 3);
 		//create the path
-		$urlGlue = $basePath . "/api/activation/?activation=" . $profileActivationToken;
+		$urlglue = $basePath . "/api/activation/?activation=" . $profileActivationToken;
 		//create the redirect link
-		$confirmLink = "https://" . $_SERVER["SERVER_NAME"] . $urlGlue;
+		$confirmLink = "https://" . $_SERVER["SERVER_NAME"] . $urlglue;
 		//compose message to send with email
 		$message = <<< EOF
 <h2> Welcome to CareerBusters</h2>
@@ -138,7 +142,7 @@ EOF;
 
 		$mailer = new Swift_Mailer($smtp);
 		//send the message
-		$numSent = $mailer->send($swiftMessage, $failedRecipients);
+		$numSent = $mailer->send($swiftMessage);
 		/**
 		 * the send method returns the number of recipients that accepted the Email
 		 * so, if the number attempted is not the number accepted, this is an Exception
