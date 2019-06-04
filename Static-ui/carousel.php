ed2787ee48f3a19c7585390c9d0022934947e3f0
Natasha Lovato <nmarshlovato@cnm.edu>
2019-06-04 16:32:35 -0600

    added staticUI-carousel & images

---
+++ Static-ui/carousel.php
@@ -0,0 +1,26 @@
+<?php require_once ("head-utils.php");?>
+
+<body>
+	<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+		<div class="carousel-inner">
+			<div class="carousel-item active">
+				<img src="images/example-job-descripition-career-busters2.jpg" class="d-block w-100" alt="Example Job Description 1">
+			</div>
+			<div class="carousel-item">
+				<img src="images/example-job-descripiton-career-busters3.jpg" class="d-block w-100" alt="Example Job Description 2">
+			</div>
+			<div class="carousel-item">
+				<img src="images/example-job-description-career-busters1.jpg" class="d-block w-100" alt="Example Job Description 3">
+			</div>
+		</div>
+		<a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+			<span class="sr-only">Previous</span>
+		</a>
+		<a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+			<span class="carousel-control-next-icon" aria-hidden="true"></span>
+			<span class="sr-only">Next</span>
+		</a>
+	</div>
+
+</body>
