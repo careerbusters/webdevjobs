5f3572c64a6cd017289f7cca939dd0a26222a480
Erik Young <youngblkraven@gmail.com>
2019-06-04 13:26:40 -0600

    added code for footer

---
+++ Static-ui/Ui-footer.php
@@ -0,0 +1,11 @@
+
+	<!-- Footer -->
+	<footer class="footer container-fluid test-center fixed-bottom">
+
+		<!-- Copyright -->
+		<div class="footer-copyright text-center py-3">© 2019 copyright by Erik Young, Tasha Lovato, Trystan Gray
+		</div>
+		<!-- Copyright -->
+
+	</footer>
+	<!-- Footer -->
