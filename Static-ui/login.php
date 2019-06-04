c41f3b2a22cb098d96167358ac329edb4e67b0ea
Natasha Lovato <nmarshlovato@cnm.edu>
2019-06-04 16:01:36 -0600

    add commits to staticUI-login

--- Static-ui/login.php
+++ Static-ui/login.php
@@ -1,24 +1,28 @@
 <?php require_once ("head-utils.php");?>
 
-	<!--Login Form with image card & Jumbotron-->
+	<!--Jumbotron-->
 	<body>
 		<section>
-			<div class="container py-2 d-flex justify-content-center">
-			<div class="card" style="width: 18rem;">
-				<img class="img-fluid mx-auto d-block my-3 rounded-circle" src="images/jackCapestone.jpg" class="card-img-top" alt="Jack Daniel">
-				<div class="card-body">
-					<p class="card-text">John Doe</p>
-				</div>
-			</div>
 			<div id="home" class="col-lg-offset-4">
 				<div class="container py-2 d-flex justify-content-center">
-					<div id="jumbotron" class="jumbotron p-3 mb-2 bg-transparent text-body .bg-transparent">
+					<div id="jumbotron" class="jumbotron p-3 mb-2 bg-transparent text-body text-align .bg-transparent">
 						<h2 class="display-1">Web Dev Career Busters Login</h2>
 					</div>
 				</div>
 			</div>
 		</section>
+
+		<!--Image Card-->
+
 		<section>
+			<div class="container py-2 d-flex justify-content-center">
+			<div class="card" style="width: 18rem;">
+				<img class="img mx-auto d-block my-3 rounded-circle" src="images/jackCapestone.jpg" class="card-img-top" alt="Jack Daniel">
+				<div class="card-body">
+					<p class="card-text">John Doe</p>
+
+				<!--Login Form-->
+
 			<form>
 				<div class="form-group">
 					<label for="exampleInputEmail1">Email address</label>
@@ -34,6 +38,8 @@
 					<label class="form-check-label" for="exampleCheck1">Remember Me</label>
 				</div>
 				<button type="submit" class="btn btn-primary btn-lg btn-block">Login</button>
+				</div>
+			</div>
 			</form>
 			</div>
 		</section>
