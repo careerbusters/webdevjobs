63e338f555fbf529883743229d059033400dd5c6
Natasha Lovato <nmarshlovato@cnm.edu>
2019-06-04 15:19:42 -0600

    edited staticUI login

--- Static-ui/login.php
+++ Static-ui/login.php
@@ -1,13 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
 
-	<!--Login Form & Jumbotron-->
+	<!--Login Form with image card & Jumbotron-->
 	<body>
 		<section>
+			<div class="container py-2 d-flex justify-content-center">
+			<div class="card" style="width: 18rem;">
+				<img class="img-fluid mx-auto d-block my-3 rounded-circle" src="images/jackCapestone.jpg" class="card-img-top" alt="Jack Daniel">
+				<div class="card-body">
+					<p class="card-text">John Doe</p>
+				</div>
+			</div>
 			<div id="home" class="col-lg-offset-4">
 				<div class="container py-2 d-flex justify-content-center">
 					<div id="jumbotron" class="jumbotron p-3 mb-2 bg-transparent text-body .bg-transparent">
-						<h2 class="display-1">WebDevCareerBusters Login</h2>
+						<h2 class="display-1">Web Dev Career Busters Login</h2>
 					</div>
 				</div>
 			</div>
@@ -29,6 +36,7 @@
 				</div>
 				<button type="submit" class="btn btn-primary btn-lg btn-block">Login</button>
 			</form>
+			</div>
 		</section>
 
 	</body>
