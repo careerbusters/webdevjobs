15e9035c7a0600ca42fa8c671e12843c2d001caf
Natasha Lovato <nmarshlovato@cnm.edu>
2019-06-05 16:28:12 -0600

    added background file

---
+++ Static-ui/background.php
@@ -0,0 +1,18 @@
+<?php require_once ("head-utils.php");?>
+
+<head>
+<style type="text/css">
+	body {
+		display: flex;
+		height: 100vh;
+		justify-content: center;
+		align-items: center;
+		background-color: #000;
+		font-family: Open Sans, sans-serif;
+	}
+</style>
+</head>
+
+<body>
+	<div class="block">Web Dev Jobs</div>
+</body>
