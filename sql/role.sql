ac5f920d1ec063dfd76ba8b3bad92ccb69ce47fd
Erik Young <youngblkraven@gmail.com>
2019-06-05 10:23:36 -0600

    fixing signup

--- sql/role.sql
+++ sql/role.sql
@@ -1,1 +1,1 @@
-insert into role (roleId, roleName) VALUES (unhex('3ef745072eff45acb41bf4269263965f'), 'recuiter');
+insert into role (roleId, roleName) VALUES (unhex('3ef745072eff45acb41bf4269263965f'), 'recruiter');
