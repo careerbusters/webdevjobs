7c29283bd0ed293a8fd803c2605ba5328a2bcbce
Natasha Lovato <nmarshlovato@cnm.edu>
2019-06-05 09:37:15 -0600

    added navbar image and navbar for home page, posting page & saved job page

---
+++ Static-ui/navbar.php
@@ -0,0 +1,35 @@
+<?php require_once("head-utils.php"); ?>
+
+<head>
+	<!--Required Meta Tags-->
+	<meta charset="UTF-8">
+	<!--<link rel="stylesheet" href="stylesheet.css"-->
+	<title>Web Dev Career Busters Login Page</title>
+</head>
+<body>
+
+	<!-- Just an image -->
+	<header>
+		<section>
+			<nav class="navbar navbar-light bg-light">
+				<a class="navbar-brand" href="#">
+					<img src="/docs/4.3/assets/brand/bootstrap-solid.svg" width="30" height="30" alt="">
+				</a>
+			</nav>
+		</section>
+		<!--Navbar-->
+		<ul class="nav justify-content-end">
+			<li class="nav-item">
+				<a class="nav-link active" href="#">Active</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="#">Link</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="#">Link</a>
+			</li>
+		</ul>
+	</header>
+
+</body>
+</html>
