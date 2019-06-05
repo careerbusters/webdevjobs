2655a8191a4b1fb9cce08e124839801f7777112c
Natasha Lovato <nmarshlovato@cnm.edu>
2019-06-05 09:17:40 -0600

    created css file

--- Static-ui/login.php
+++ Static-ui/login.php
@@ -1,47 +1,50 @@
-<?php require_once ("head-utils.php");?>
+<?php require_once("head-utils.php"); ?>
 
-	<!--Jumbotron-->
-	<body>
-		<section>
-			<div id="home" class="col-lg-offset-4">
-				<div class="container py-2 d-flex justify-content-center">
-					<div id="jumbotron" class="jumbotron p-3 mb-2 bg-transparent text-body text-align .bg-transparent">
-						<h2 class="display-1">Web Dev Career Busters Login</h2>
+<!--Jumbotron-->
+<body>
+	<section>
+		<div id="home" class="col-lg-offset-4">
+			<div class="container py-2 d-flex justify-content-center text-content-center">
+				<div id="jumbotron" class="jumbotron p-3 mb-2 bg-transparent text-body text-align .bg-transparent">
+					<div class="row">
+						<h1 class="display-2">Web Dev Career Busters Login</h1>
 					</div>
 				</div>
 			</div>
-		</section>
+		</div>
+	</section>
 
-		<!--Image Card-->
+	<!--Image Card-->
 
-		<section>
-			<div class="container py-2 d-flex justify-content-center">
+	<section>
+		<div class="container py-2 d-flex justify-content-center">
 			<div class="card" style="width: 18rem;">
-				<img class="img mx-auto d-block my-3 rounded-circle" src="images/jackCapestone.jpg" class="card-img-top" alt="Jack Daniel">
+				<img class="img mx-auto d-block my-3 rounded-circle" src="images/jackCapestone.jpg" class="card-img-top"
+					  alt="Jack Daniel">
 				<div class="card-body">
 					<p class="card-text">John Doe</p>
 
-				<!--Login Form-->
+					<!--Login Form-->
 
-			<form>
-				<div class="form-group">
-					<label for="exampleInputEmail1">Email address</label>
-					<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-							 placeholder="Enter email">
-				</div>
-				<div class="form-group">
-					<label for="exampleInputPassword1">Password</label>
-					<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-				</div>
-				<div class="form-group form-check">
-					<input type="checkbox" class="form-check-input" id="exampleCheck1">
-					<label class="form-check-label" for="exampleCheck1">Remember Me</label>
-				</div>
-				<button type="submit" class="btn btn-primary btn-lg btn-block">Login</button>
+					<form>
+						<div class="form-group">
+							<label for="exampleInputEmail1">Email address</label>
+							<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+									 placeholder="Enter email">
+						</div>
+						<div class="form-group">
+							<label for="exampleInputPassword1">Password</label>
+							<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+						</div>
+						<div class="form-group form-check">
+							<input type="checkbox" class="form-check-input" id="exampleCheck1">
+							<label class="form-check-label" for="exampleCheck1">Remember Me</label>
+						</div>
+						<button type="submit" class="btn btn-primary btn-lg btn-block">Login</button>
 				</div>
 			</div>
 			</form>
-			</div>
-		</section>
+		</div>
+	</section>
 
-	</body>
+</body>
