ff22e3658706858f3728ceadb9254b078f049fa5
Erik Young <youngblkraven@gmail.com>
2019-06-06 17:54:56 -0600

    working on NavBar

--- app/src/shared/components/NavBar.js
+++ app/src/shared/components/NavBar.js
@@ -6,27 +6,26 @@ export const NavBar = () => (
 <header>
 	<nav className="navbar navbar-light bg-light">
 		<a className="navbar-brand"/a>
-			<button className="fa-user-circle fa-2x" data-target="Profile Icon">
-			</button>
+			<a className="fa-user-circle fa-2x" data-target="Profile Icon">
+			</a>
 
 
 		<ul className="nav justify-content-end">
 			<li className="nav-item">
 				<a className="nav-link active" href="https://bootcamp-coders.cnm.edu/~eyoung21/webdevjobs/Static-ui/navbar.php"/a>
-					<button className="fas fa-home fa-2x text-dark" data-target="Home Page Icon">
-					</button>
+					<a className="fas fa-home fa-2x text-dark" data-target="Home Page Icon">
+					</a>
 
 
 		<li className="nav-item">
 			<a className="nav-link active" href="https://bootcamp-coders.cnm.edu/~eyoung21/webdevjobs/Static-ui/navbar.php"/a>
-	<button className="fas fa-users fa-2x text-dark" data-target="Recruiter Job Posting Page Icon">
-		</button>
+	<a className="fas fa-users fa-2x text-dark" data-target="Recruiter Job Posting Page Icon">
+		</a>
 
 
 	<li className="nav-item">
 		<a className="nav-link active" href="https://bootcamp-coders.cnm.edu/~eyoung21/webdevjobs/Static-ui/navbar.php"/a>
-			<button className="fas fa-briefcase fa-2x text-dark" data-target="Saved Job Icon">
-			</button>
+			<a className="fas fa-briefcase fa-2x text-dark" data-target="Saved Job Icon"</a>
 	</li>
 </ul>
 </nav>
