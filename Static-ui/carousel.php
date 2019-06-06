b2bfba050dcbc9ca6300802328141abbbc8a13fb
Natasha Lovato <nmarshlovato@cnm.edu>
2019-06-06 08:03:19 -0600

    edited carousel

--- Static-ui/carousel.php
+++ Static-ui/carousel.php
@@ -4,13 +4,13 @@
 	<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
 		<div class="carousel-inner">
 			<div class="carousel-item active">
-				<img src="images/example-job-descripition-career-busters2.jpg" class="d-block w-100" alt="Example Job Description 1">
+				<img src="/epic/images/jackCapestone.jpg" height="500" width="400" class="d-block w-100" alt="Example Job Description 1">
 			</div>
 			<div class="carousel-item">
-				<img src="images/example-job-descripiton-career-busters3.jpg" class="d-block w-100" alt="Example Job Description 2">
+				<img src="/epic/images/example-job-description-career-busters2.jpg" height="500" width="400" class="d-block w-100" alt="Example Job Description 2">
 			</div>
 			<div class="carousel-item">
-				<img src="images/example-job-description-career-busters1.jpg" class="d-block w-100" alt="Example Job Description 3">
+				<img src="/epic/images/example-job-description-career-busters3.jpg" height="500" width="400" class="d-block w-100" alt="Example Job Description 3">
 			</div>
 		</div>
 		<a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
