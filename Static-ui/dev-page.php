bc7e87f1b8e5d154a1192e53df0760727ab84f08
Natasha Lovato <nmarshlovato@cnm.edu>
2019-06-07 00:28:23 -0600

    push commit for dev page revision

--- Static-ui/dev-page.php
+++ Static-ui/dev-page.php
@@ -1,27 +1,56 @@
-<?php require_once("head-utils.php");?>
-<?php require_once("navbar.php");?>
-<?php require_once ("background.php");?>
-<?php require_once("Ui-footer.php");?>
+<?php require_once("head-utils.php"); ?>
+<?php require_once("navbar.php"); ?>
+<?php require_once("background.php"); ?>
+<?php require_once("Ui-footer.php"); ?>
 
 <body>
 	<!--Dev Page Header-->
 	<section>
-		<div id="dev" class="container text-center">
+		<div class="container text-center">
 			<h2>Developer Hub</h2>
 		</div>
 
 		<!--User Cards-->
-		<div class="container justify-content-center">
+		<div id="dev" class="container justify-content-center">
 			<div class="row justify-content-center">
-		<div class="card-text text-center" style="width: 50rem;">
-			<img src="capstoneProfilePeep2.jpg" class="card-img-top" alt="Profile Example 1">
-			<div class="col-md-6 position-static p-4 pl-md-0">
-				<h5 class="mt-0">Will Iamm</h5>
-				<p class="text-wrap text-center">Seasoned Coder. Full-Stack Web Developer. I wear many hats. Making things and building teams!! </p>
-				<a href="#" class="stretched-link">More</a>
+				<div class="card-text text-center" style="width: 50rem;">
+					<img src="capstoneProfilePeep2.jpg" class="card-img-top" alt="Profile Example 1">
+					<div class="col-md-6 position-static p-4 pl-md-0">
+						<h5 class="mt-0">Will Iamm</h5>
+						<p class="text-wrap text-center">Seasoned Coder. Full-Stack Web Developer. I wear many hats. Making
+							things and building teams!! </p>
+						<a href="#" class="stretched-link">See More...</a>
+					</div>
+				</div>
 			</div>
 		</div>
+		<div id="dev" class="container justify-content-center">
+			<div class="row justify-content-center">
+				<div class="card-text text-center" style="width: 50rem;">
+					<img src="capstoneProfilePeep3.jpg" class="card-img-top" alt="Profile Example 1">
+					<div class="col-md-6 position-static p-4 pl-md-0">
+						<h5 class="mt-0">Scott Pickles</h5>
+						<p class="text-wrap text-center">STrained and educated in the fundamental concepts and technologies
+							(languages, tools, and frameworks) required of a frontend (UX, Wireframing, UI) and backend (Data
+							Design, ERD, DDL) developer.</p>
+						<a href="#" class="stretched-link">See More...</a>
+					</div>
+				</div>
+			</div>
 		</div>
+		<div id="dev" class="container justify-content-center">
+			<div class="row justify-content-center">
+				<div class="card-text text-center" style="width: 50rem;">
+					<img src="profilePeep4.jpg" class="card-img-top" alt="Profile Example 1">
+					<div class="col-md-6 position-static p-4 pl-md-0">
+						<h5 class="mt-0">Jenny Woo</h5>
+						<p class="text-wrap text-center">I am a software developer skilled in data visualization and
+							JavaScript frameworks. I bring a unique perspective to programming due to my diverse portfolio of
+							professional experiences. I also excel as a liaison between coders & non-technical audiences.</p>
+						<a href="#" class="stretched-link">See More...</a>
+					</div>
+				</div>
+			</div>
 		</div>
 	</section>
 
