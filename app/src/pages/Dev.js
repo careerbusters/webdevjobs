8bc7bcdf7da65493111381d2ebd6d62bf6754391
Erik Young <youngblkraven@gmail.com>
2019-06-08 19:10:59 -0600

    created pages dev and saved

---
+++ app/src/pages/Dev.js
@@ -0,0 +1,62 @@
+import React from "react";
+export const Dev = () => (
+	<>
+<body>
+
+	<section>
+		<div className="container text-center">
+			<h2>Developer Hub</h2>
+		</div>
+
+
+		<div id="dev" className="container justify-content-center">
+			<div className="row justify-content-center">
+				<div className="card-text text-center" style="width: 50rem;">
+					<img src="capstoneProfilePeep2.jpg" className="card-img-top" alt="Profile Example 1"/>
+						<div className="col-md-6 position-static p-4 pl-md-0">
+							<h5 className="mt-0">Will Iamm</h5>
+							<p className="text-wrap text-center">Seasoned Coder. Full-Stack Web Developer. I wear many hats.
+								Making
+								things and building teams!! </p>
+							<a href="#" className="stretched-link">See More...</a>
+						</div>
+				</div>
+			</div>
+		</div>
+		<div id="dev" className="container justify-content-center">
+			<div className="row justify-content-center">
+				<div className="card-text text-center" style="width: 50rem;">
+					<img src="capstoneProfilePeep3.jpg" className="card-img-top" alt="Profile Example 1"/>
+						<div className="col-md-6 position-static p-4 pl-md-0">
+							<h5 className="mt-0">Scott Pickles</h5>
+							<p className="text-wrap text-center">STrained and educated in the fundamental concepts and
+								technologies
+								(languages, tools, and frameworks) required of a frontend (UX, Wireframing, UI) and backend
+								(Data
+								Design, ERD, DDL) developer.</p>
+							<a href="#" className="stretched-link">See More...</a>
+						</div>
+				</div>
+			</div>
+		</div>
+		<div id="dev" className="container justify-content-center">
+			<div className="row justify-content-center">
+				<div className="card-text text-center" style="width: 50rem;">
+					<img src="profilePeep4.jpg" className="card-img-top" alt="Profile Example 1"/>
+						<div className="col-md-6 position-static p-4 pl-md-0">
+							<h5 className="mt-0">Jenny Woo</h5>
+							<p className="text-wrap text-center">I am a software developer skilled in data visualization and
+								JavaScript frameworks. I bring a unique perspective to programming due to my diverse portfolio
+								of
+								professional experiences. I also excel as a liaison between coders & non-technical
+								audiences.</p>
+							<a href="#" className="stretched-link">See More...</a>
+						</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+</body>
+	</>
+);
