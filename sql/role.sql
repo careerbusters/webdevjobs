29a8074131676441a334d2f07d62e9c9e7a99082
Erik Young <youngblkraven@gmail.com>
2019-06-11 11:01:36 -0600

    removed vardump from signup api

--- sql/role.sql
+++ sql/role.sql
@@ -1,1 +1,6 @@
-insert into role (roleId, roleName) VALUES (unhex('3ef745072eff45acb41bf4269263965f'), 'recruiter');
+insert into role (roleId, roleName) VALUES
+(unhex('3ef745072eff45acb41bf4269263965f'), 'recruiters'),
+(unhex('74fa46bd8bec485da7561f1b9d8112c9'), 'developer'),
+(unhex('990090de35574eea95cd1fa29d852ec8'), 'freelancer'),
+(unhex('6580ee20e20d4b05a95916c5a4688e0f'), 'entrepreneurs'),
+(unhex('b404f20f9d274edf9305943e95d5bd10'), 'other tech fields')
