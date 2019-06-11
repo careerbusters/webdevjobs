c91dbb489cab74d06407247fb707026a380a706b
Erik Young <youngblkraven@gmail.com>
2019-06-11 15:15:08 -0600

    added profile for freelancer

--- app/src/pages/Dev.js
+++ app/src/pages/Dev.js
@@ -21,20 +21,22 @@ export const DevPage = () => (
 					<div className="col-sm-8 justify-content-center">
 						<div id="dev">
 								<div className="card bg-light border-primary">
-									<img src="/imgs/morganCaptain.jpg" height="auto" width="auto" className="card-img-top p-1"
-										  alt="Profile Example 1"/>
 									<h5 className="text-center p-3">Marty Bonacci</h5>
-									<p className="text-center mx-3">Seasoned Coder. Full time DAD! Full-Stack Web Developer. I wear many hats.
-										Making things and building teams!!</p>
-									<a href="#" className="stretched-link text-right mb-2 mx-3">Read More...</a>
+									<p className="text-center mx-3">
+										I’ve been lucky enough to have the pleasure of Designing and Programming Websites for the last handful of years.
+										I first worked as a Graphic Designer at a Printing Facility, designing everything from Business Cards and Logos to flyers, brochures, and huge banners.
+										Eventually I was asked to make a website - and I did what every programmer does: I do searches using Google to figure out how to fix my problems and errors. Since then, I have been learning the ins and outs of the Web Design world - specifically Front End Development.
+										In Web Design I satiate my desire to visually design something while stretching my Programming wings at the same time. I like to work with the Wordpress platform, focusing on creating websites that result in a pleasing browsing experience for the user - making sure that the website is viewable on whatever sized screen, fast, and beautiful.
+										Outside of work, I enjoy growing Carnivorous Plants and learning how to make stuff with wood.My Skills: Print / Graphic Design, 5 Years Experience, adobe Photoshop / Illustrator, Knowledge of Design Principles
+										Experience working at a Print Facility, Web Design & Development class, 8 Years Experience HTML, CSS/SCSS/SASS , Javascript, JQuery, PHP, Wordpress Expert,Bootstrap, Responsive/Mobile Design
+										Performance Optimization, 1 year working with React</p>
 								</div>
 							</div>
 						</div>
 					</div>
 				</div>
 		</body>
-		<br/>
-		<br/>
+
 		<Footer/>
 	</>
 );
