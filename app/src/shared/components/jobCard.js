58d5066755baf316d3e9878fc1707218ff8bb93a
Trystan Gray <trystangray7@gmail.com>
2019-06-11 19:16:49 -0600

    adding job card

--- app/src/shared/components/jobCard.js
+++ app/src/shared/components/jobCard.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Moment from "react-moment";
+
 
 const JobCardComponent = ({postings}) => {
 	 console.log(postings);
@@ -12,14 +14,13 @@ const JobCardComponent = ({postings}) => {
 						<div className="card-header justify-content-left">
 							{`${index.postingTitle} - ${index.postingCompanyName}`}
 						</div>
+
 						<div className="card-body">
-							<h5 className="card-title text-left mt-1">{`${index.postingDate} apply until ${index.postingEndDate}`}</h5>
-							<p className="card-text">I am web dev, I go code code code. I am web dev, I go code code code. I am
-								web
-								dev, I go code code code. I am web dev, I go code code code.</p>
+							<h5 className="card-title text-left mt-1"><Moment unix>{index.postingDate}</Moment> apply until</h5>
+							<p className="card-text">{`${index.postingContent}`}</p>
 						</div>
 						<div className="card-footer text-muted">
-							Albuquerque, New Mexico
+							{`${index.postingLocation}`}
 						</div>
 					</div>
 				)
