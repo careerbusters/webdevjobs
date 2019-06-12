4a700b1193525832f63ae54d80da44c629bc6b95
Trystan Gray <trystangray7@gmail.com>
2019-06-12 16:02:43 -0600

    made job page work

--- app/src/pages/Jobs.js
+++ app/src/pages/Jobs.js
@@ -1,44 +1,47 @@
-import React from "react";
-import {Footer} from "../shared/components/Footer";
+import React, {useEffect} from 'react';
+import {getAllPostings} from "../shared/actions/";
+import {connect} from "react-redux";
 import {NavBar} from "../shared/components/NavBar";
-export const Jobs = () => (
-	<>
-		<NavBar/>
-<body className="bg-secondary pb-5 ">
-	<div className="container text-center">
-		<div id="jumbotron"
-			  className="jumbotron p-3 mb-2 bg-transparent text-body text-align .bg-transparent">
-			<div className="row justify-content-center">
-				<h1 className="display-2 font-weight-bold a">Job Postings</h1>
-			</div>
-		</div>
-	</div>
-	<section>
-		<div className="container bg-dark mb-5 p-5">
-			<div className="row justify-content-center my-5">
-				<div className="col-lg-9">
-					<div className="card text-center">
-						<div className="card-header justify-content-left">
-							Fullstack Web Developer - RS21
-						</div>
-						<div className="card-body">
-							<h5 className="card-title text-left mt-1">Job Valid Date</h5>
-							<p className="card-text">I am web dev, I go code code code. I am web dev, I go code code code. I am
-								web
-								dev, I go code code code. I am web dev, I go code code code.</p>
-							<a href="#" className="btn btn-dark float-right">Expand Profile</a>
-						</div>
-						<div className="card-footer text-muted">
-							Albuquerque, New Mexico
+import {Footer} from "../shared/components/Footer";
+import {JobCard} from "../shared/components/jobCard";
+import {httpConfig} from "../shared/misc/http-config";
+
+
+const JobsComponent = ({posting, getAllPostings}) => {
+	useEffect(() => {
+			getAllPostings();
+		},
+		[getAllPostings]
+	);
+	return (
+		<>
+			<NavBar/>
+			<body className="bg-secondary pb-5 ">
+				<div className="container text-center">
+					<div id="jumbotron"
+						  className="jumbotron p-3 mb-2 bg-transparent text-body text-align .bg-transparent">
+						<div className="row justify-content-center">
+							<h1 className="display-2 font-weight-bold a">Job Postings</h1>
 						</div>
 					</div>
 				</div>
-			</div>
-		</div>
-	</section>
-</body>
-		<Footer/>
+				<section>
+					<div className="container bg-dark mb-5 p-5">
+						<div className="row justify-content-center my-5">
+							<div className="col-lg-9">
+								<JobCard postings={posting}/>
+							</div>
+						</div>
+					</div>
+				</section>
+			</body>
+			<Footer/>
+		</>
+	)
+};
 
-	</>
-);
+const mapStateToProps = ({posting}) => {
+	return {posting};
+};
+export const Jobs = connect(mapStateToProps, {getAllPostings})(JobsComponent);
 
